b563a249e3f7e3c97dbe600087bbbc88f33ffb9c
luboslenco <lubos.lenco@gmail.com>
2018-08-06 22:49:32 +0200

    Fix bone update

--- celshade/Sources/arm/FirstPersonController.hx
+++ celshade/Sources/arm/FirstPersonController.hx
@@ -24,6 +24,20 @@ class FirstPersonController extends CameraController {
 	var soundStep0:kha.Sound = null;
 	var soundStep1:kha.Sound = null;
 
+	var xVec = Vec4.xAxis();
+	var zVec = Vec4.zAxis();
+	var angle = 0.0;
+	var nextFrameRot = 0.0;
+	var anim:BoneAnimation;
+	var q = new Quat();
+	var mat = Mat4.identity();
+
+	var nextIdle = false;
+	var firingTime = 0.0;
+	var speed = 1.0;
+	var dir = new Vec4();
+	var state = "idle";
+
 	public function new() {
 		super();
 		notifyOnInit(function() {
@@ -41,6 +55,9 @@ class FirstPersonController extends CameraController {
 			iron.data.Data.getSound("step1.wav", function(sound:kha.Sound) {
 				soundStep1 = sound;
 			});
+
+			anim = findAnimation(object.getChild("Armature"));
+			anim.notifyOnUpdate(updateBones);
 		});
 	}
 
@@ -53,13 +70,81 @@ class FirstPersonController extends CameraController {
 		return null;
 	}
 
-	var xVec = Vec4.xAxis();
-	var zVec = Vec4.zAxis();
-	var angle = 0.0;
-	var nextFrameRot = 0.0;
-	var anim:BoneAnimation;
-	var q = new Quat();
-	var mat = Mat4.identity();
+	function updateBones() {
+
+		// Fetch bone
+		var bone1 = anim.getBone("mixamorig:LeftForeArm");
+		var bone2 = anim.getBone("mixamorig:RightForeArm");
+
+		// Fetch bone matrix - this is in local bone space for now
+		var m1 = anim.getBoneMat(bone1);
+		var m2 = anim.getBoneMat(bone2);
+		var m1b = anim.getBoneMatBlend(bone1);
+		var m2b = anim.getBoneMatBlend(bone2);
+		var a1 = anim.getAbsMat(bone1.parent);
+		var a2 = anim.getAbsMat(bone2.parent);
+
+		// Rotate hand bones to aim with gun
+		// Some raw math follows..
+		var tx = m1._30;
+		var ty = m1._31;
+		var tz = m1._32;
+		m1._30 = 0;
+		m1._31 = 0;
+		m1._32 = 0;
+		mat.getInverse(a1);
+		q.fromAxisAngle(mat.right(), angle);
+		m1.applyQuat(q);
+		m1._30 = tx;
+		m1._31 = ty;
+		m1._32 = tz;
+		
+		var tx = m2._30;
+		var ty = m2._31;
+		var tz = m2._32;
+		m2._30 = 0;
+		m2._31 = 0;
+		m2._32 = 0;
+		mat.getInverse(a2);
+		var v = mat.right();
+		v.mult(-1);
+		q.fromAxisAngle(v, -angle);
+		m2.applyQuat(q);
+		m2._30 = tx;
+		m2._31 = ty;
+		m2._32 = tz;
+
+		// Animation blending is in progress, we need to rotate those bones too
+		if (m1b != null && m2b != null) {
+			var tx = m1b._30;
+			var ty = m1b._31;
+			var tz = m1b._32;
+			m1b._30 = 0;
+			m1b._31 = 0;
+			m1b._32 = 0;
+			mat.getInverse(a1);
+			q.fromAxisAngle(mat.right(), angle);
+			m1b.applyQuat(q);
+			m1b._30 = tx;
+			m1b._31 = ty;
+			m1b._32 = tz;
+			
+			var tx = m2b._30;
+			var ty = m2b._31;
+			var tz = m2b._32;
+			m2b._30 = 0;
+			m2b._31 = 0;
+			m2b._32 = 0;
+			mat.getInverse(a2);
+			var v = mat.right();
+			v.mult(-1);
+			q.fromAxisAngle(v, -angle);
+			m2b.applyQuat(q);
+			m2b._30 = tx;
+			m2b._31 = ty;
+			m2b._32 = tz;
+		}
+	}
 
 	function preUpdate() {
 		if (Input.occupied || !body.ready) return;
@@ -84,93 +169,10 @@ class FirstPersonController extends CameraController {
 			angle += d;
 			nextFrameRot = -mouse.movementY / 250 * rotationSpeed;
 		}
-		
-		anim = findAnimation(object.getChild("Armature"));
-		
-		// Fetch bone
-		var bone1 = anim.getBone("mixamorig:LeftForeArm");
-		var bone2 = anim.getBone("mixamorig:RightForeArm");
-		
-		// Fetch bone matrix - this is in local bone space for now
-		var m1 = anim.getBoneMat(bone1);
-		var m2 = anim.getBoneMat(bone2);
-		var m1b = anim.getBoneMatBlend(bone1);
-		var m2b = anim.getBoneMatBlend(bone2);
-		var a1 = anim.getAbsMat(bone1.parent);
-		var a2 = anim.getAbsMat(bone2.parent);
-
-		anim.notifyOnUpdate(function() {
-			// Rotate hand bones to aim with gun
-			// Some raw math follows..
-			var tx = m1._30;
-			var ty = m1._31;
-			var tz = m1._32;
-			m1._30 = 0;
-			m1._31 = 0;
-			m1._32 = 0;
-			mat.getInverse(a1);
-			q.fromAxisAngle(mat.right(), angle);
-			m1.applyQuat(q);
-			m1._30 = tx;
-			m1._31 = ty;
-			m1._32 = tz;
-			
-			var tx = m2._30;
-			var ty = m2._31;
-			var tz = m2._32;
-			m2._30 = 0;
-			m2._31 = 0;
-			m2._32 = 0;
-			mat.getInverse(a2);
-			var v = mat.right();
-			v.mult(-1);
-			q.fromAxisAngle(v, -angle);
-			m2.applyQuat(q);
-			m2._30 = tx;
-			m2._31 = ty;
-			m2._32 = tz;
-
-			// Animation blending is in progress, we need to rotate those bones too
-			if (m1b != null && m2b != null) {
-				var tx = m1b._30;
-				var ty = m1b._31;
-				var tz = m1b._32;
-				m1b._30 = 0;
-				m1b._31 = 0;
-				m1b._32 = 0;
-				mat.getInverse(a1);
-				q.fromAxisAngle(mat.right(), angle);
-				m1b.applyQuat(q);
-				m1b._30 = tx;
-				m1b._31 = ty;
-				m1b._32 = tz;
-				
-				var tx = m2b._30;
-				var ty = m2b._31;
-				var tz = m2b._32;
-				m2b._30 = 0;
-				m2b._31 = 0;
-				m2b._32 = 0;
-				mat.getInverse(a2);
-				var v = mat.right();
-				v.mult(-1);
-				q.fromAxisAngle(v, -angle);
-				m2b.applyQuat(q);
-				m2b._30 = tx;
-				m2b._31 = ty;
-				m2b._32 = tz;
-			}
-		});
 
 		if (mouse.moved) transform.rotate(zVec, -mouse.movementX / 250 * rotationSpeed);
 		body.syncTransform();
 	}
-
-	var nextIdle = false;
-	var firingTime = 0.0;
-	var speed = 0.8;
-	var dir = new Vec4();
-	var state = "idle";
 
 	function update() {
 		if (!body.ready) return;
@@ -191,6 +193,10 @@ class FirstPersonController extends CameraController {
 				state = "run";
 				anim.play(state, null, 0.2);
 			}
+
+			var kb = iron.system.Input.getKeyboard();
+			if (kb.down("shift")) speed = 1.6;
+			else speed = 1.0;
 
 			dir.mult(speed * 5);
 			body.activate();
